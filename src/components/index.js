fe0162a520271e83b5045c7b9ca879851ccf12ec
Ujwol Aryal <ujwolaryal@gmail.com>
2026-03-11 13:08:08 +0545

    Remove DoorReveal component and refactor App component structure for improved layout and accessibility

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,4 @@
 // Export all components for easier imports
-export { default as DoorReveal } from './DoorReveal'
 export { default as Navigation } from './Navigation'
 export { default as Hero } from './Hero'
 export { default as About } from './About'
